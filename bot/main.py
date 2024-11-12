9aabd58f8c3c941df53a75ebda27b3e0c2196076
leUnknown64 <dergermankind67@gmail.com>
2024-11-12 11:40:27 -0500

    Working on receiving data from http

--- bot/main.py
+++ bot/main.py
@@ -5,9 +5,22 @@
 #
 # ===---------------------------------------------------------------------=== #
 
-def main():
-    print("Client!")
+from aiohttp import ClientSession
+import asyncio
 
+async def obtain_id() -> dict:
+    async with ClientSession() as session:
+        url = 'http://localhost:8080/init'
+        method = 'GET'
+        async with session.request(method, url) as response:
+            json_reply = await response.json()
+            return json_reply['id']
+
+
+def main():
+    id = asyncio.run(obtain_id())
+    print(id)
+ 
 
 if __name__ == "__main__":
     main()
